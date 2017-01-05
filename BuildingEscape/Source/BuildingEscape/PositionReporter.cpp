64364a0b6c7146d1b8988312c8fd49f6ced803b8
redbean <redbeanmouse@gmail.com>
2017-01-05 18:10:20 +0900

    class creation

---
+++ BuildingEscape/Source/BuildingEscape/PositionReporter.cpp
@@ -0,0 +1,37 @@
+// Copyright Ben Tristem 2016.
+
+#include "BuildingEscape.h"
+#include "PositionReporter.h"
+#include "Engine.h"
+
+
+// Sets default values for this component's properties
+UPositionReporter::UPositionReporter()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	bWantsBeginPlay = true;
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UPositionReporter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UPositionReporter::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+{
+	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
+
+	// ...
+}
+
